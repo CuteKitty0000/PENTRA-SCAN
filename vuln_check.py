05e6c5c5caaa09490edec500362d9199fcbea2ee
Navaneetha Krishnan M <144705877+CuteKitty0000@users.noreply.github.com>
2024-03-18 05:27:26 +0000

    modified:   vuln_check.py

--- vuln_check.py
+++ vuln_check.py
@@ -1,41 +1,59 @@
 import subprocess
-import winreg
+import platform
 import psutil
+
 from vuln_info import vulnerabilities
 
-def is_defender_running():
-    return any("MsMpEng.exe" in proc.name() for proc in psutil.process_iter())
-
 def is_firewall_enabled():
-    output = subprocess.run(["netsh", "advfirewall", "show", "allprofiles", "state"], capture_output=True, text=True)
-    return "Firewall state: On" in output.stdout
+    if platform.system() == "Windows":
+        output = subprocess.run(["netsh", "advfirewall", "show", "allprofiles", "state"], capture_output=True, text=True)
+        return "Firewall state: On" in output.stdout
+    elif platform.system() == "Linux":
+        try:
+            output = subprocess.run(["ufw", "status"], capture_output=True, text=True)
+            return "Status: active" in output.stdout
+        except FileNotFoundError:
+            return False
+    else:
+        return False
 
 def is_uac_enabled():
-    key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System")
-    value, _ = winreg.QueryValueEx(key, "EnableLUA")
-    return value == 1
+    if platform.system() == "Windows":
+        import winreg
+        key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System")
+        value, _ = winreg.QueryValueEx(key, "EnableLUA")
+        return value == 1
+    else:
+        return False
 
 def is_rdp_enabled():
-    output = subprocess.run(["reg", "query", "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server"], capture_output=True, text=True)
-    return "fDenyTSConnections    REG_DWORD    0x0" in output.stdout
+    if platform.system() == "Windows":
+        output = subprocess.run(["reg", "query", "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server"], capture_output=True, text=True)
+        return "fDenyTSConnections    REG_DWORD    0x0" in output.stdout
+    else:
+        return False
 
 def is_secureboot_enabled():
-    output = subprocess.run(["bcdedit"], capture_output=True, text=True)
-    return "Secure Boot State                 On" in output.stdout
+    if platform.system() == "Windows":
+        output = subprocess.run(["bcdedit"], capture_output=True, text=True)
+        return "Secure Boot State                 On" in output.stdout
+    else:
+        return False
 
 def is_bitlocker_enabled():
-    output = subprocess.run(["manage-bde", "-status"], capture_output=True, text=True)
-    return "Fully Encrypted" in output.stdout
+    if platform.system() == "Windows":
+        output = subprocess.run(["manage-bde", "-status"], capture_output=True, text=True)
+        return "Fully Encrypted" in output.stdout
+    else:
+        return False
 
 def check_security():
-    print("Security Assessment Report:")
+    print("\n\nSecurity Assessment Report:")
     print("=" * 30)
     for vulnerability, info, mitigation in vulnerabilities:
         print("\n", "-" * 50)
         print(f"{vulnerability}: {info}")
-        if "Windows Defender" in vulnerability and not is_defender_running():
-            print(f"   - Vulnerability detected. {mitigation}")
-        elif "Windows Firewall" in vulnerability and not is_firewall_enabled():
+        if "Windows Firewall" in vulnerability and not is_firewall_enabled():
             print(f"   - Vulnerability detected. {mitigation}")
         elif "User Account Control (UAC)" in vulnerability and not is_uac_enabled():
             print(f"   - Vulnerability detected. {mitigation}")
